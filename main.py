8c399f4f7696e4196f20f7dbc76954b586b28266
Hoops01 <shaun.hooper01@gmail.com>
2016-05-26 20:22:49 +1000

    Got rid of some whitespace, updated randomchoose (still can get stuck). Got rid of main lines.

--- main.py
+++ main.py
@@ -6,23 +6,22 @@ Created on Sun May 22 15:22:09 2016
 """
 
 import numpy as np
+import random
 
 def minbar(target, racklist):
     barlist = []
     remainder = target
     while remainder <> 0:
-        print racklist
         maxweight = max(racklist)
-        print maxweight
         if maxweight <= remainder:
             # Yes, then add maxweight to bar and remove from rack
             remainder = remainder - maxweight
             barlist.append(maxweight)
             print "Added ", maxweight, "to bar, ", remainder, "to go"
         racklist.remove(maxweight)
-        
+
     return barlist
-    
+
 def addbar(target, barlist, racklist):
     discardlist = []
     onbar = sum(barlist)
@@ -44,24 +43,45 @@ def addbar(target, barlist, racklist):
     print racklist
     return
 
-def randomload(target, barlist, racklist):
-    import random
-    onbar = sum(barlist)
+def randomchoose(target, barlist, racklist):
+    """
+    Randomly start loading the bar until the target weight is met
+    """
+
+    remaining = target - sum(barlist)
     counter = 0
-    while onbar != target:
-        onbar = sum(barlist)
-        remaining = target - onbar
-        r = random.choice(racklist)
-        if r <= remaining:
-            barlist.append(r)
-            racklist.remove(r)
+    while remaining >= 0:
+        plate = random.choice(racklist)
+        print "Choosing plate ", plate, " remaining ", remaining
+        if plate <= remaining:
+            barlist.append(plate)
+            racklist.remove(plate)
+            remaining = remaining - plate
+            print "Added ", plate
+            print "remaining ", remaining
+            print "racklist", racklist
+            if remaining//min(racklist)*min(racklist) != remaining:
+                # there isn't going to be a way of completing this,
+                # so remove last plate
+                barlist.remove(plate)
+                racklist.add(plate)
+                remaining = remaining + plate
+            #print "HERE"
+        remaining = target - sum(barlist)
         counter += 1
         if counter > 1000:
+            print "spun out"
             break
+
+def emptybar(barlist,racklist):
+    for plate in barlist:
+        barlist.remove(plate)
+        racklist.append(plate)
+    #racklist.sort(reverse=True)
 
 racklist = [20,20,10,5,5,2.5,2.5,1.25,1.25]
 barlist = []
 
-randomload(41.25,barlist,racklist)
-print sorted(racklist)
-print sorted(barlist), sum(barlist)
+#randomload(41.25,barlist,racklist)
+#print sorted(racklist)
+#print sorted(barlist), sum(barlist)
